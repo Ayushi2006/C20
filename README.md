d17fbba292b0fd99370f09ed49e8787dc0f4d467
Ayushi2006 <73694291+Ayushi2006@users.noreply.github.com>
2020-11-22 16:56:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C20
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,39 @@
+var movingRect, fixedRect;
+
+
+function setup() {
+
+  createCanvas(1200,800);
+
+  movingRect=createSprite(400, 200, 80, 30);
+  movingRect.shapeColor="orange";
+  movingRect.debug=true;
+
+  fixedRect=createSprite(500, 100, 80, 50);
+  fixedRect.shapeColor="orange";
+  fixedRect.debug=true;
+
+}
+
+function draw() {
+
+  background(0);
+  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
+    && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    && fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+      movingRect.shapeColor="green";
+      fixedRect.shapeColor="green";
+    }
+    else{
+      movingRect.shapeColor="orange";
+      fixedRect.shapeColor="orange";
+    }
+    
+  drawSprites();
+
+}
